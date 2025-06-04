837b245731c65d460b7ee20d3826319ad32b70f4
JedidiahMusa <jedidiahsimon3@gmail.com>
2025-06-04 12:24:43 +0100

    .

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -9,19 +9,28 @@ const TodoList = () => {
     setTodos(updatedTodos);
   }
 
+
   function addTodo() {
     if(inputValue.trim() === '') return;
     const newTodo ={
       id: Date.now(),
-      task: inputValue
+      task: inputValue,
+      completed: false
     }
     setTodos([...todos, newTodo]);
     setInputValue('');
   }
+
+  function handleDone(idToToggle){
+    const updatedTodo = todos.map(todo=>
+      todo.id === idToToggle ? {...todo, completed: !todo.completed} : todo
+    );
+    setTodos(updatedTodo)
+  }
   
   return (
     <div className="p-8">
-      <h1 className="uppercase text-2xl font-bold text-center py-8">
+      <h1 className="uppercase text-3xl font-bold text-center py-8">
         list of things to do
       </h1>
       <div className="flex w-full gap-4 ">
@@ -30,25 +39,23 @@ const TodoList = () => {
           value={inputValue}
           onChange={(e)=>setInputValue(e.target.value)}
           placeholder="Enter a Task"
-          className="border-1 focus:outline-none focus:border-gray-500 focus:border-2 border-gray-400 p-2 flex-1 rounded-2xl  "
+          className="border-1 focus:outline-none focus:border-gray-500 focus:border-2 border-gray-400 py-3 px-4 flex-1 rounded-2xl  "
         />
-        <button onClick={addTodo} className="bg-gray-700 p-2 rounded-2xl transition-all duration-200 hover:bg-gray-800 text-[.8rem] text-white uppercase font-bold ">
+        <button onClick={addTodo} className="bg-gray-700 py-2 px-3 rounded-2xl transition-all duration-200 hover:bg-gray-800 text-[.8rem] text-white uppercase font-bold ">
           add todo
         </button>
       </div>
 
       <div>
         {todos.map((todo)=>(
-          <div key={todo.id} className="flex my-8 items-center justify-between border-1 py-8 px-2 border-gray-200 rounded-2xl  w-full">
-            <p className="px-2 wrap-anywhere" >
+          <div key={todo.id} className={`flex my-8 items-center justify-between  py-8 px-2 rounded-2xl  w-full ${todo.completed ? ' border-1 border-green-200': ' border-1 border-gray-200'}`}>
+            <p className={`px-2 wrap-anywhere ${todo.completed ? 'line-through text-gray-400' : ''}`} >
             {todo.task}
         </p>
-        <div className="min-w-[10rem]">
-          <button className="mr-4 hover:bg-green-500 p-3 bg-green-400 cursor-pointer transition-all duration-200 uppercase text-[.8rem] font-bold rounded-2xl">done</button>
-        <button onClick={() => handleDelete(todo.id)} className="p-3 bg-red-400 hover:bg-red-500 transition-all cursor-pointer duration-200 uppercase text-[.8rem] font-bold rounded-2xl">delete</button>
+        <div className="flex justify-end ">
+          <button onClick={()=>handleDone(todo.id)} className={`mr-4 p-3 rounded-2xl cursor-pointer transition-all duration-200 uppercase text-[.8rem] font-bold rounded-2xl${todo.completed ? ' bg-gray-400' : ' bg-green-400'}`}>{todo.completed? 'Completed': 'done'}</button>
+        <button onClick={() => handleDelete(todo.id)} className={`p-3 transition-all cursor-pointer duration-200 uppercase text-[.8rem] font-bold rounded-2xl ${todo.completed ? 'bg-gray-400' : 'bg-red-400'}`}>delete</button>
         </div>
-    
-        
           </div>
           
       ))}
